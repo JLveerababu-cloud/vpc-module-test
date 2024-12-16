518a94a5b7e4286d2cd394264271d77d291abb37
veerababu <jlveerababu99@gmail.com>
2024-12-16 15:37:21 +0530

    terraform

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+     project_name = var.project_name
+     environment = var.environment
+     common_tags = var.common_tags
+     public_subnet_cidrs = var.public_subnet_cidrs
+     private_subnet_cidrs =  var.private_subnet_cidrs
+     datbse_subnet_cidrs = var.datbase_subnet_cidrs
+}
